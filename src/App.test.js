92ac7b951eeae034df1af23db0ee77419e6fc1b5
Nikhil Devarakonda <nikhildevarakonda520@gmail.com>
2025-04-09 18:21:20 -0400

    Made some changes in the test file and degrade the axios version

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders loading profiles message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const loadingElement = screen.getByText(/loading profiles/i);
+  expect(loadingElement).toBeInTheDocument();
 });
